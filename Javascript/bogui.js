1ea96b0a685e2e786620f1d38542395fbc6fbc70
Guillermo Rivero <alu0100702149.ull.edu.es>
2014-09-26 11:55:43 +0100

    Añadido soporte para importar imágenes al canvas

--- Javascript/bogui.js
+++ Javascript/bogui.js
@@ -3,3 +3,31 @@
 Autores: Guillermo Rivero Rodríguez y Boris Ballester Hernández"
 
 */
+
+var img;
+var canvas;
+var context;
+
+function init() {
+	canvas = document.getElementById("canvas");
+	context = canvas.getContext("2d");
+	document.getElementById("selectorFichero").addEventListener("change", readImage, false);
+}
+
+function readImage() {
+    if ( this.files && this.files[0] ) {
+        var FR= new FileReader();
+        FR.onload = function(e) {
+           img = new Image();
+           img.onload = function() {
+             context.drawImage(img, 0, 0);
+           };
+    img.src = e.target.result;
+        };       
+        FR.readAsDataURL( this.files[0] );
+    }
+}
+
+
+
+
