2d84c943de7e21088156fe09f12cce3808363fbe
Guillermo Rivero Rodríguez <guillermotegueste@gmail.com>
2014-10-17 17:21:11 +0100

    Añadida funcion para cambiar el tamaño de las imagenes que sean mayores que unas dimenciones especificadas

--- js/bogui.js
+++ js/bogui.js
@@ -6,6 +6,8 @@ Autores: Guillermo Rivero Rodríguez y Boris Ballester Hernández"
 
 var imagen;
 var objetosBogui = [];
+var maxWidth = 600;
+var maxHeight = 600;
 
 $(document).ready(function() {
 	//canvas = document.getElementById("canvas");
@@ -37,10 +39,15 @@ function Bogui(img, id) {
 	this.imagen = img;
 	
 	var dialogoAux; 
+
+	this.reducirImagen = reducirImagen;
+
 	this.RGBA2BW = RGBA2BW;
 
 	dialogoAux = document.createElement("div");
 	dialogoAux.setAttribute("id", "dialogo"+this.ident);
+	dialogoAux.setAttribute("height", maxHeight);
+	dialogoAux.setAttribute("width", maxWidth);
 	document.getElementById('workspace').appendChild(dialogoAux);
 	
 	this.imgCanvas;
@@ -63,8 +70,9 @@ function Bogui(img, id) {
 	this.ctx = this.imgCanvas.getContext('2d');
 	this.ctx.drawImage(this.imagen, 0, 0);
 
-	this.RGBA2BW();
 	
+	this.reducirImagen();
+	this.RGBA2BW();
 }
 
 function borrarObjetoBogui(id){
@@ -79,6 +87,32 @@ var i = 0;
 
 }
 
+
+function reducirImagen(){
+
+  var canvasCopy = document.createElement("canvas");
+  var copyContext = canvasCopy.getContext("2d");
+
+  // Create original image
+
+  // Determine new ratio based on max size
+  var ratio = 1;
+  if(this.imagen.width > maxWidth)
+    ratio = maxWidth / this.imagen.width;
+  else if(this.imagen.height > maxHeight)
+    ratio = maxHeight / this.imagen.height;
+
+  // Draw original image in second canvas
+  canvasCopy.width = this.imagen.width;
+  canvasCopy.height = this.imagen.height;
+  copyContext.drawImage(this.imagen, 0, 0);
+
+  // Copy and resize second canvas to first canvas
+  this.imgCanvas.width = this.imagen.width * ratio;
+  this.imgCanvas.height = this.imagen.height * ratio;
+  this.ctx.drawImage(canvasCopy, 0, 0, canvasCopy.width, canvasCopy.height, 0, 0, this.imgCanvas.width, this.imgCanvas.height);
+
+}
 
 function RGBA2BW(){
 
