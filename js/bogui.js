c27a6666c8625ae6ea1e6a94e9f56c59653809ce
Boris Ballester Hernández <borbalher@gmail.com>
2014-11-16 19:48:29 +0000

    Transformacion por tramos finalizado

--- js/bogui.js
+++ js/bogui.js
@@ -158,69 +158,73 @@ $(document).ready(function() {
 	});		
 
 	$("#transformacionTramos").click(function() {
-		var dialog, form;
-		
-		$("body").append("<div id=\"dialog\"></div>");
-		dialog = $( "#dialog" ).dialog({
-			title: "Transformacion lineal por tramos:",
-			height: 200,
-			width: 350,
-			modal: true,
-			buttons: {
-			Ok:function() {
-				  //COGEMOS LOS VALORES
-				  var numTramos =  $( this ).find( '#tramosSlider' ).slider( "value" );
-				  $(this).dialog( "close" );
-				  $(this).remove();
-				  transformacionLinealTramosDialog(numTramos);
-			},
-			Cancel: function() {
-				  $(this).dialog( "close" );
-				  $(this).remove();
-			}
-			},
-			dialogClass: 'no-close' 	
-		});
-		
-		dialog.append("<form><fieldset><p><label for=\"tramosSpinner\">Numero de tramos:</label><input id=\"tramosSpinner\" name=\"tramosValue\" type=\"text\"></p><div id=\"tramosSlider\"></div></fieldset></form>");;
-
-		form = dialog.find( "form" ).on( "submit", function( event ) {
-		  event.preventDefault();
-		});		
-		
-		var tramosSpinner = $( "#tramosSpinner" ).spinner({
-			min: 1,
-			max: 254,
-			step: 1,
-			start: 1,
-			stop: (function (event, ui) {
-				$( "#tramosSlider" ).slider( "value", $(this).spinner('value') );
-			}),
-			spin: (function(event, ui ){
-			$( "#tramosSlider" ).slider( "value", ui.value );
-			})
-		}).on('input', function () {
-			 var val = this.value,
-				 $this = $(this),
-				 max = $this.spinner('option', 'max'),
-				 min = $this.spinner('option', 'min');
-				 if (!val.match(/^\d*$/)) val = 1; //we want only number, no alpha
-			 this.value = (val > max) ? max : val;
-		 });	
+		if(typeof objetosBogui[objetoActual] == 'undefined'){
+			mostrarError("No se puede ejecutar el comando sin una imagen seleccionada"); 
+		}else{
+			var dialog, form;
 			
-		$( "#tramosSlider" ).slider({
-		  range: "min",
-		  value: 1,
-		  min: 1,
-		  autofocus: "autofocus",
-		  max: 254,
-		  slide: function( event, ui ) {
-			tramosSpinner.spinner( "value", ui.value );
-		  }
-		});
-		tramosSpinner.spinner( "value", $( "#tramosSlider" ).slider( "value" ));
-
-		dialog.dialog();
+			$("body").append("<div id=\"dialog\"></div>");
+			dialog = $( "#dialog" ).dialog({
+				title: "Transformacion lineal por tramos:",
+				height: 200,
+				width: 350,
+				modal: true,
+				buttons: {
+					Ok:function() {
+						//COGEMOS LOS VALORES
+						var numTramos =  $( this ).find( '#tramosSlider' ).slider( "value" );
+						$(this).dialog( "close" );
+						$(this).remove();
+						transformacionLinealTramosDialog(numTramos);
+					},
+					Cancel: function() {
+						$(this).dialog( "close" );
+						$(this).remove();
+					}
+				},
+				dialogClass: 'no-close' 	
+			});
+			
+			dialog.append("<form><fieldset><p><label for=\"tramosSpinner\">Numero de tramos:</label><input id=\"tramosSpinner\" name=\"tramosValue\" type=\"text\"></p><div id=\"tramosSlider\"></div></fieldset></form>");;
+			
+			form = dialog.find( "form" ).on( "submit", function( event ) {
+				event.preventDefault();
+			});		
+			
+			var tramosSpinner = $( "#tramosSpinner" ).spinner({
+				min: 1,
+				max: 254,
+				step: 1,
+				start: 1,
+				stop: (function (event, ui) {
+					$( "#tramosSlider" ).slider( "value", $(this).spinner('value') );
+				}),
+				spin: (function(event, ui ){
+					$( "#tramosSlider" ).slider( "value", ui.value );
+				})
+				}).on('input', function () {
+				var val = this.value,
+				$this = $(this),
+				max = $this.spinner('option', 'max'),
+				min = $this.spinner('option', 'min');
+				if (!val.match(/^\d*$/)) val = 1; //we want only number, no alpha
+				this.value = (val > max) ? max : val;
+			});	
+			
+			$( "#tramosSlider" ).slider({
+				range: "min",
+				value: 1,
+				min: 1,
+				autofocus: "autofocus",
+				max: 254,
+				slide: function( event, ui ) {
+					tramosSpinner.spinner( "value", ui.value );
+				}
+			});
+			tramosSpinner.spinner( "value", $( "#tramosSlider" ).slider( "value" ));
+			
+			dialog.dialog();
+		}
 	});		
 	
 	$("#instaDownloadButton").click(function() {
@@ -366,8 +370,8 @@ function transformacionLinealTramosDialog(numTramos){
 		$("body").append("<div id=\"dialog\"></div>");
 		dialog = $( "#dialog" ).dialog({
 			title: "Especifique los tramos:",
-			height: 360,
-			width: 285,
+			height: 515,
+			width: 700,
 			modal: true,
 			buttons: {
 			Ok:function() {
@@ -375,18 +379,18 @@ function transformacionLinealTramosDialog(numTramos){
 					var puntoAnterior = -1;
 					var counter = 0;
 					var puntos = [];
-					puntos.push([0,0]);
-									
-					while(error == false && counter < numTramos)
+
+					while(error == false && counter <= numTramos)
 					{
 						var a =  $( this ).find( '#a'+counter ).spinner( "value" );
 						var b =  $( this ).find( '#b'+counter ).spinner( "value" );			
-						if(a == null || b == null || b <= a || a < puntoAnterior )
+						if((a != null && b != null) && ( a <= puntoAnterior) )
 						{
 							puntos = null;
 							error = true;							
-						}else{
-							puntoAnterior = b;
+						}else
+						{
+							puntoAnterior = a;
 							counter = counter+1;
 							puntos.push([a,b]);	
 						}
@@ -394,17 +398,12 @@ function transformacionLinealTramosDialog(numTramos){
 					
 					if(error == false)
 					{
-						puntos.push([255,255]);
-						
-						$(this).append('#contenedorGraficaTransformacionLineal');
 						transformacionLinearPorTramos(objetosBogui[objetoActual], puntos);
-						//FUNCION TRANSFORMACION
-						//CIERRE
 						$(this).dialog( "close" );
 						$(this).remove();
 					}else
 					{
-						mostrarError("Introduzca el número correcto de parametros");
+						mostrarError("Hay parametros incorrectos. El tramo debe ser progesivo");
 					}
 
 			},
@@ -416,24 +415,60 @@ function transformacionLinealTramosDialog(numTramos){
 			dialogClass: 'no-close' 	
 		});
 		
-		var tramos = "<form><fieldset><table><tbody>";
+		var tramos = "<form class=\"quarter floatleft\"><fieldset><table><tbody>";
 		
 		for(i=0; i <= numTramos; i++){
 				tramos = tramos + "<tr><td><label>Punto "+(i+1)+":</label></td><td><input id=\"a"+i+"\" name=\"a"+i+"\" type=\"text\"></td><td><input id=\"b"+i+"\" name=\"b"+i+"\" type=\"text\"></td></tr>";
 		}
 		
 		dialog.append(tramos+"</tbody></table></form>");;
-
+		dialog.append("<div id=\"graficaTramos\" class=\"threequarter floatleft\"></div>");
 		form = dialog.find( "form" ).on( "submit", function( event ) {
 		  event.preventDefault();
 		});			
-
+		
+	    $('#graficaTramos').highcharts({
+       	chart: {
+            type: 'scatter'
+        },
+        title: {
+            text: 'Tramos'
+        },
+        subtitle: {
+            text: 'Especificacion de los tramos del usuario.'
+        },
+        xAxis: {
+            min: 0,
+            max: 255            
+        },
+        yAxis: {
+            min: 0,
+            max: 255,        
+			title: null,
+            tickInterval: 15
+        },
+        legend: {
+            enabled: false
+        },
+        plotOptions: {
+            series: {
+                lineWidth: 1,
+            }
+        },
+        series: [{
+            data: []
+        }]	
+		});		
+	
 		for(i=0; i <= numTramos; i++){
 			var tramoASpinner = $( "#a"+i ).spinner({
 				min: 0,
 				max: 255,
 				step: 1,
-				start: 0
+				start: 0,
+				change: function (event, ui) {
+					actualizarGraficaTramos(numTramos);
+				}		
 			}).on('input', function () {
 				 var val = this.value,
 					 $this = $(this),
@@ -447,7 +482,10 @@ function transformacionLinealTramosDialog(numTramos){
 				min: 0,
 				max: 255,
 				step: 1,
-				start: 0
+				start: 0,
+				change: function (event, ui) {
+					actualizarGraficaTramos(numTramos);
+				}
 			}).on('input', function () {
 				 var val = this.value,
 					 $this = $(this),
@@ -458,13 +496,43 @@ function transformacionLinealTramosDialog(numTramos){
 			 });				 
 			 
 		}
-		
-		if(form.height() <= 274){
-			var newHeigth = form.height() + 110;
-			dialog.dialog("option", "height", newHeigth); 
-		}
 
+		//AÑADIMOS LOS PRIMEROS PUNTOS
+		$("#a0").spinner( "value",0 );
+		$("#a0").spinner( "disable" );
+		$("#b0").spinner( "value",0 );
+		$("#b0").spinner( "disable" );
+		
+		$("#a"+numTramos).spinner( "value",255 );
+		$("#a"+numTramos).spinner( "disable" );
+		$("#b"+numTramos).spinner( "value",255);
+		$("#b"+numTramos).spinner( "disable" );		
+		
+	
+		actualizarGraficaTramos(numTramos);
 		dialog.dialog();
+}
+
+function actualizarGraficaTramos(numTramos){	
+	var puntoAnterior = 0;
+	var counter = 1;
+	var puntos = [];
+	puntos.push([0,0]);
+	
+	while(counter <= numTramos)
+	{
+		var a =  $( "#dialog" ).find( '#a'+counter ).spinner( "value" );
+		var b =  $( "#dialog" ).find( '#b'+counter ).spinner( "value" );	
+		
+		if((a != null && b != null) && ( a > puntoAnterior) )
+		{
+			puntoAnterior = a;
+			puntos.push([a,b]);
+		}
+		counter = counter+1;
+	}
+	puntos.push([255,255]);
+	$("#graficaTramos").highcharts().series[0].setData(puntos);
 }
 
 function readImage() {
@@ -1256,7 +1324,6 @@ function especificarHistograma(objetoBoguiActual, objetoBoguiOrigen){
 
 	aplicarFuncionTransferencia(objetoBoguiActual, funcionTransferencia);
 }
-
 
 function transformacionLinearPorTramos(objetoBoguiActual, tramos){
 	
